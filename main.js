ed42816867ef7e21070616cb053a7fd66d00f228
Kaan Taş <73135188+Totikajs@users.noreply.github.com>
2021-01-05 10:38:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,91 +9,6 @@ const YAML = require("yawn-yaml/cjs")
 const { load } = require("js-yaml")
 const db = require('megadb')
 const voiceState = new db.crearDB('VoiceState');
-require('./utils/eventLoader.js')(totika);
-// Totika Tarafından Geliştirilmiştir.
-
-totika.commands = new Collection();
-totika.aliases = new Collection();
-let log = console.log;
-fs.readdir('./komutlar/', (err, files) => {
-    if (err) console.error(err);
-    log(`${files.length} komut yüklenecek.`);
-    files.forEach(f => {
-        let props = require(`./komutlar/${f}`);
-        log(`Yüklenen komut: ${props.help.name}.`);
-        totika.commands.set(props.help.name, props);
-        props.conf.aliases.forEach(alias => {
-            totika.aliases.set(alias, props.help.name);
-        });
-    });
-});
-
-totika.reload = command => {
-    return new Promise((resolve, reject) => {
-        try {
-            delete require.cache[require.resolve(`./komutlar/${command}`)];
-            let cmd = require(`./komutlar/${command}`);
-            totika.commands.delete(command);
-            totika.aliases.forEach((cmd, alias) => {
-                if (cmd === command) totika.aliases.delete(alias);
-            });
-            totika.commands.set(command, cmd);
-            cmd.conf.aliases.forEach(alias => {
-                totika.aliases.set(alias, cmd.help.name);
-            });
-            resolve();
-        } catch (e) {
-            reject(e);
-        }
-    });
-};
-
-totika.load = command => {
-    return new Promise((resolve, reject) => {
-        try {
-            let cmd = require(`./komutlar/${command}`);
-            totika.commands.set(command, cmd);
-            cmd.conf.aliases.forEach(alias => {
-                totika.aliases.set(alias, cmd.help.name);
-            });
-            resolve();
-        } catch (e) {
-            reject(e);
-        }
-    });
-};
-
-
-
-
-totika.unload = command => {
-    return new Promise((resolve, reject) => {
-        try {
-            delete require.cache[require.resolve(`./komutlar/${command}`)];
-            let cmd = require(`./komutlar/${command}`);
-            totika.commands.delete(command);
-            totika.aliases.forEach((cmd, alias) => {
-                if (cmd === command) totika.aliases.delete(alias);
-            });
-            resolve();
-        } catch (e) {
-            reject(e);
-        }
-    });
-};
-
-totika.elevation = message => {
-    if (!message.guild) {
-        return;
-    }
-    let permlvl = 0;
-    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-    if (message.author.id === settings.sahip) permlvl = 4;
-    return permlvl;
-};
-
-
 let config = new YAML(fs.readFileSync("./config.yaml").toString()).json
 
 console.log(config.server)
@@ -135,4 +50,6 @@ totika.on("voiceStateUpdate", async(oldState, newState) => {
 })
 
 
+// Totika Tarafından Geliştirilmiştir.
+// Totika Tarafından Geliştirilmiştir.
 
